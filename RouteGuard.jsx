a2d54bda303a0914f10dc56d42cee5467b7f2d39
Henry Laurente Jr. <junolaurente@gmail.com>
2026-04-24 12:22:18 +0800

    Add files via upload

---
+++ RouteGuard.jsx
@@ -0,0 +1,14 @@
+import { Navigate, useLocation } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
+
+export default function RouteGuard({ children }) {
+  const { user } = useAuth()
+  const location = useLocation()
+
+  
+  if (user?.role === 'USER' && location.pathname.includes('deleted-items')) {
+    return <Navigate to="/" replace />
+  }
+
+  return children
+}
